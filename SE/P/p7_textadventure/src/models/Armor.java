b40b32de0e6b3f0a205999cb51a14525a2121bd8
Simon Weissenbach <simon.weissenbach@bmw.de>
2025-05-21 15:08:50 +0200

    add p7

---
+++ SE/P/p7_textadventure/src/models/Armor.java
@@ -0,0 +1,34 @@
+package models;
+
+public class Armor extends Item {
+    private double defense;
+
+    // constructor
+    public Armor(String name, double weight, double defense) {
+        super(name, weight);
+        this.defense = defense;
+    }
+
+    public Armor() {
+        this("Kartoffelsack", 20.0, 1.0);
+    }
+
+    // methods
+    @Override
+    public void printItem() {
+        System.out.println("id: " + id + " name: " + name + " weight: " + weight + " defense: " + defense);
+    }
+
+    public double getDefenseValue() {
+        //more logic?
+        return defense;
+    }
+
+    // getters and setters
+    public double getDefense() {
+        return defense;
+    }
+    public void setDefense(double defense) {
+        this.defense = defense;
+    }
+}
